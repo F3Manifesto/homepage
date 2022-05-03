7e185b3262246214920068debc7ecbd4c4490402
Jameslin <jameslin@Jameslins-iMac-Pro.local>
2022-05-03 10:51:37 -0400

    feat: update guide section

--- src/components/GuideSection/index.js
+++ src/components/GuideSection/index.js
@@ -1,11 +1,96 @@
 import React from 'react'
 import styles from './styles.module.scss'
 
+const guideImages = [
+  '/images/homepage/guide/guide-1.jpg',
+  '/images/homepage/guide/guide-2.jpg',
+  '/images/homepage/guide/guide-3.jpg',
+]
+
 const GuideSection = () => {
   return (
     <div className={styles.guideWrapper}>
       <div className={styles.title}>
         Znarky's Poignant Guide to NFTs
+      </div>
+
+      <div className={styles.animationWrapper}>
+        <div className={styles.animationReverse}>
+          <div className={styles.marquee}>
+            <div className={styles.animationFrame}>
+              {
+                guideImages.map((item, index) => {
+                  return (
+                    <img key={index} src={guideImages[index]} />
+                  )
+                })
+              }
+              {
+                guideImages.map((item, index) => {
+                  return (
+                    <img key={guideImages.length + index} src={guideImages[index]} />
+                  )
+                })
+              }
+            </div>
+          </div>
+          <div className={styles.marquee}>
+            <div className={styles.animationFrame}>
+              {
+                guideImages.map((item, index) => {
+                  return (
+                    <img key={index} src={guideImages[index]} />
+                  )
+                })
+              }
+              {
+                guideImages.map((item, index) => {
+                  return (
+                    <img key={guideImages.length + index} src={guideImages[index]} />
+                  )
+                })
+              }
+            </div>
+          </div>
+        </div>
+        <div className={styles.animationReverse}>
+          <div className={styles.marquee}>
+            <div className={styles.animationFrame}>
+              {
+                guideImages.map((item, index) => {
+                  return (
+                    <img key={index} src={guideImages[index]} />
+                  )
+                })
+              }
+              {
+                guideImages.map((item, index) => {
+                  return (
+                    <img key={guideImages.length + index} src={guideImages[index]} />
+                  )
+                })
+              }
+            </div>
+          </div>
+          <div className={styles.marquee}>
+            <div className={styles.animationFrame}>
+              {
+                guideImages.map((item, index) => {
+                  return (
+                    <img key={index} src={guideImages[index]} />
+                  )
+                })
+              }
+              {
+                guideImages.map((item, index) => {
+                  return (
+                    <img key={guideImages.length + index} src={guideImages[index]} />
+                  )
+                })
+              }
+            </div>
+          </div>
+        </div>
       </div>
 
       <a
